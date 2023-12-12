cfa2f7a3fc1402cc6af788654e25e48d94385300
Gaëtan Cassiers <gaetan.cassiers@iaik.tugraz.at>
2023-12-12 15:05:25 +0100

    Add latency info to agema results

--- agema/summarize_areas.py
+++ agema/summarize_areas.py
@@ -1,5 +1,6 @@
 import argparse
 import csv
+import re
 import json
 from pathlib import Path
 
@@ -8,17 +9,22 @@ GE = 0.798 # Nangate45
 
 def parse_reports(reports, module):
     for report in reports:
+        report_dir = report.parent
         with open(report) as f:
             l = json.load(f)
-        _, order = str(report.parent.name).rsplit('_', 1)
+        _, order = str(report_dir.name).rsplit('_', 1)
         order = int(order[1:])
         areaum2 = float(l['design']['area'])
         area_ge = areaum2/GE
-        rnd_bits = 34 * order * (order+1) # 34 HPC3 gadgets
-        area_rnd_ge = 33.1708333 * rnd_bits # constant manually copied
+        with open(report_dir / f'{module}_HPC3_Pipeline_d{order}.v') as f:
+            verilog = f.read()
+        latency = int(re.search(r'the circuit has (\d+) register stage', verilog).group(1))
+        rnd_bits = int(re.search(r'input \[(\d+):0\] Fresh ;', verilog).group(1)) + 1
+        area_rnd_ge = 33.1708333 * rnd_bits # RNG bit-area copied from synthesis results
         yield {
                 'design': module,
                 'nshares': order+1,
+                'latency': latency,
                 'area_ge': area_ge,
                 'area_ge_wrng': area_ge + area_rnd_ge,
                 'solve_time': '',
@@ -38,7 +44,7 @@ def main():
     args = cli().parse_args()
     res = list(parse_reports(args.reports, args.module))
     with open(args.outcsv, 'w') as csvfile:
-        fields=['design', 'nshares', 'area_ge', 'area_ge_wrng', 'solve_time', 'rnd_bits']
+        fields=['design', 'nshares', 'latency', 'area_ge', 'area_ge_wrng', 'solve_time', 'rnd_bits']
         writer = csv.DictWriter(csvfile, fieldnames=fields)
         writer.writeheader()
         writer.writerows(res)
